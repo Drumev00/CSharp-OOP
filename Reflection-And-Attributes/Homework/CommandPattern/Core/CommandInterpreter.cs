d465b75592f13b7c36e8998e78dd5e1874de2db5
Alexander <drumev00@gmail.com>
2019-12-31 18:00:52 +0200

    Solved homework about reflection and attributes

---
+++ Reflection-And-Attributes/Homework/CommandPattern/Core/CommandInterpreter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+using CommandPattern.Core.Contracts;
+
+namespace CommandPattern.Core
+{
+	public class CommandInterpreter : ICommandInterpreter
+	{
+		public string Read(string args)
+		{
+			string[] tokens = args.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+			string commandName = $"{tokens[0]}Command";
+
+			Assembly assembly = Assembly.GetCallingAssembly();
+			Type[] types = assembly.GetTypes();
+			Type typeToCreate = types.FirstOrDefault(t => t.Name == commandName);
+
+
+			object classInstance = Activator.CreateInstance(typeToCreate, new object[] { });
+			ICommand command = (ICommand)classInstance;
+			string result = command.Execute(tokens.Skip(1).ToArray());
+
+			return result;
+		}
+	}
+}
---
+++ Reflection-And-Attributes/Homework/ValidationAttributes/Utilities/Validator.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using System.Reflection;
+
+using ValidationAttributes.Attributes;
+
+namespace ValidationAttributes.Utilities
+{
+	public static class Validator
+	{
+		public static bool IsValid(object obj)
+		{
+			PropertyInfo[] properties = obj.GetType().GetProperties();
+			foreach (var prop in properties)
+			{
+				MyValidationAttribute[] attributes = prop
+					.GetCustomAttributes()
+					.Where(a => a is MyValidationAttribute)
+					.Cast<MyValidationAttribute>()
+					.ToArray();
+				foreach (MyValidationAttribute attribute in attributes)
+				{
+					if (!attribute.IsValid(prop.GetValue(obj)))
+					{
+						return false;
+					}
+				}
+			}
+			return true;
+		}
+	}
+}
